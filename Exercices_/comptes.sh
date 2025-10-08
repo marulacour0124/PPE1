76da9ac0aa2fc6bd26acef2d93d92d0458d4bc48
marulacour0124 <pruneaulisa@gmail.com>
2025-10-08 18:15:31 +0200

    mise a jour du script plus simple et court

--- Exercices_/comptes.sh
+++ Exercices_/comptes.sh
@@ -1,18 +1,8 @@
-!/usr/bin/env bash
+!/bin/bash
 
-YEARS=(2016 2017 2018)
+annees=(2016 2017 2018)
 
-for year in "${YEARS[@]}"; do
-  if [ -d "$year" ]; then
-    search_path="$year"
-    include='*.ann'
-  else
-    search_path='.'
-    include="*${year}*.ann"
-  fi
-
- 
-  count=$(grep -R --include="$include" -h 'Location' "$search_path" 2>/dev/null | wc -l)
-
-  printf "%s : %d\n" "$year" "$count"
+for an in "${annees[@]}"; do
+    count=$(grep -h "Location" *.ann | wc -l)
+    echo "$an : $count"
 done
