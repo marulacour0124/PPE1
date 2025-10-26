91e979661e44090b276ef53923388cef2baf12e2
marulacour0124 <pruneaulisa@gmail.com>
2025-10-26 12:18:45 +0100

    Ajout du dossier miniprojet avec ses trois sous-dossiers

---
+++ miniprojet/programmes/miniprojet.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <fichier>"
+    exit 1
+fi
+while read -r line;
+do
+	echo ${line};
+done < "$1";
+
+n=0
+while read -r url; do
+    [[ -z "$url" ]] && continue
+
+    ((n++))
+
+    # Code HTTP
+    code_http=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+
+    # Correction si erreur et URL sans http(s)
+    if [[ "$code_http" == "000" && "$url" != http* ]]; then
+        url="https://$url"
+        code_http=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+    fi
+
+    # Encodage
+    encodage=$(curl -sI "$url" | grep -i "content-type" | grep -oE "charset=[^ ;]*" | cut -d= -f2)
+    if [ -z "$encodage" ]; then
+        encodage="inconnu"
+    fi
+
+    # Nombre de mots
+    nb_mots=$(curl -s "$url" | wc -w)
+
+	echo -e "${n}\t${url}\t${nb_mots}\t${code_http}\t${encodage}"
+done < "$1"
+
